a26d8f3413e58f0d3d2c1282e9981ae1ad8e0474
TherRipper482 <santiagogustavo882@gmail.com>
2025-03-28 23:59:47 -0600

    18 Log out

--- djangobnb_backend/useraccount/urls.py
+++ djangobnb_backend/useraccount/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from dj_rest_auth.jwt_auth import get_refresh_view
-from dj_rest_auth.resgistration.views import RegisterView
+from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from rest_framework_simplejwt.views import TokenVerifyView
 
